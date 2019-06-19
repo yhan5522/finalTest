7928aa6fca1c9d5d35127611e1708bb661fe793c
yhan5522 <gks963@naver.com>
2019-06-20 05:04:21 +0900

    select 값 1차 출력

--- MyBootApp3/src/main/java/com/yena/springboot/HeloController.java
+++ MyBootApp3/src/main/java/com/yena/springboot/HeloController.java
@@ -1,42 +1,59 @@
 package com.yena.springboot;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.yena.springboot.repositories.MyDataRepository;
 
 @Controller
 public class HeloController {
 	
-	@Autowired
-	MyDataRepository repository;
+	
+	/**
+	 * 	@fn		public ModelAndView index(@ModelAttribute("formModel" MyData mydata,
+	 * 									  ModelAndView mav)
+	 *	@brief	초기 페이지 설정
+	 *	@details
+	 *
+	 *	@author	한예나
+	 *	@date	2019-06-20
+	 
+	 *	@param
+	 *	@remark	웹 페이지를 불러오기 위한 초기페이지 설정 [2019-06-20; 한예나]
+	 */
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
-	public ModelAndView index(@ModelAttribute("formModel") MyData mydata,
-							  ModelAndView mav){
+	public ModelAndView index(ModelAndView mav){
 		
 		mav.setViewName("index");
-		mav.addObject("msg","this is sample content.");
-		Iterable<MyData> list = repository.findAll();
-		mav.addObject("data", list);
+		return mav;
+		
+	}
+
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam(value="schoolfood",required=false)String[] schoolfood,
+							 @RequestParam("address")String address,
+							 ModelAndView mav){
+
+		String food = "";
+		try {
+			food += schoolfood[0];
+			for (int i=1; i<schoolfood.length; i++) {
+				food+="," + schoolfood[i];
+			}
+		} catch (NullPointerException e) {
+			food += "null";
+		}
+		
+		
+		mav.addObject("food", food);
+		mav.addObject("address", address);
+		mav.setViewName("index");
 		return mav;
 		
 	}
 	
-	@RequestMapping(value="/", method=RequestMethod.POST)
-	@Transactional(readOnly=false)
-	public ModelAndView form(@ModelAttribute("formModel") MyData mydata,
-							 ModelAndView mav) {
-		
-		repository.saveAndFlush(mydata);
-		return new ModelAndView("redirect:/");
-	
-	}
 }
 
