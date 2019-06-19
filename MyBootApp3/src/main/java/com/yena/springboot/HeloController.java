370478bb5ce4df488559f3b82082bb39ba34fc6e
yhan5522 <gks963@naver.com>
2019-06-20 03:08:54 +0900

    HeloController.java 생성

--- MyBootApp3/src/main/java/com/yena/springboot/HeloController.java
+++ MyBootApp3/src/main/java/com/yena/springboot/HeloController.java
@@ -1,9 +1,9 @@
 package com.yena.springboot;
 
-import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
