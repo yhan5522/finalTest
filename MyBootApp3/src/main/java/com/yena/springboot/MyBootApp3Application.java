5f20ff9dd82dbac8b6b3ae118365cd34fd889b13
yhan5522 <gks963@naver.com>
2019-06-20 00:03:50 +0900

    프로젝트생성

--- MyBootApp3/src/main/java/com/yena/springboot/MyBootApp3Application.java
+++ MyBootApp3/src/main/java/com/yena/springboot/MyBootApp3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyBootAppApplication {
+public class MyBootApp3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyBootAppApplication.class, args);
+		SpringApplication.run(MyBootApp3Application.class, args);
 	}
 
 }
--- MyBootApp3/src/test/java/com/yena/springboot/MyBootApp3ApplicationTests.java
+++ MyBootApp3/src/test/java/com/yena/springboot/MyBootApp3ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MyBootAppApplicationTests {
+public class MyBootApp3ApplicationTests {
 
 	@Test
 	public void contextLoads() {
